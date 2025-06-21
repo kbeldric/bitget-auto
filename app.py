962b2a9197575fca3a69dd4363ebfad119e8835e
kbeldric <101498463+kbeldric@users.noreply.github.com>
2025-06-21 11:31:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,8 @@ def webhook():
 
     return {"status": "ignored"}
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
